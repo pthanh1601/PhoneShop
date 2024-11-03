983a5bdc4ee3d6f6b68bb2a321e06ae6c081094f
Quang <quangphanminh0@gmail.com>
2024-11-03 09:37:13 +0700

    feature/QLKH

--- PhoneShop/Data/Hshop2023Context.cs
+++ PhoneShop/Data/Hshop2023Context.cs
@@ -53,7 +53,8 @@ public partial class Hshop2023Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-VMBD6LCV\\MSSQLSERVER01;Database=Hshop2023;Integrated Security=True;Trust Server Certificate=True");
+        //=> optionsBuilder.UseSqlServer("Server=LAPTOP-VMBD6LCV\\MSSQLSERVER01;Database=Hshop2023;Integrated Security=True;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-OB1FCG4;Database=master;Integrated Security=True;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+chưa tạo nav nên muốn xem danh sach khách hàng thì thêm /KhachHang/DanhSachKhachHang sau localhost
+phải đăng nhập quyền admin để xem
+chức năng thêm hình bị lỗi và đang nghiên cứu thêm để sửa
+/KhachHang/DanhSachKhachHang
 PHÂN CÔNG
 * Trang admin
   - Thoại: Quản lý hóa đơn, chi tiết hóa đơn
