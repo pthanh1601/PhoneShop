c83635694c7b6a208109d9cb2cb85d90b41528b0
pham-vu-thoai <96470189+pham-vu-thoai@users.noreply.github.com>
2024-11-05 16:19:12 +0700

    test push code chứ chưa làm gì

--- PhoneShop/Data/Hshop2023Context.cs
+++ PhoneShop/Data/Hshop2023Context.cs
@@ -51,9 +51,17 @@ public partial class Hshop2023Context : DbContext
 
     public virtual DbSet<YeuThich> YeuThiches { get; set; }
 
+//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//         => optionsBuilder.UseSqlServer("Server=LAPTOP-VMBD6LCV\\MSSQLSERVER01;Database=Hshop2023;Integrated Security=True;Trust Server Certificate=True");
+     // Sử dụng chuỗi kết nối từ appsettings.json
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-VMBD6LCV\\MSSQLSERVER01;Database=Hshop2023;Integrated Security=True;Trust Server Certificate=True");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Hshop2023;User Id=SA;Password=MyStrongPass123;Trust Server Certificate=True");
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
