746020181e95bbb90b96e1a831457df6e9c95d21
phuthanh <trinthanh160102@gmail.com>
2024-11-18 14:33:07 +0700

    â

--- PhoneShop/ViewModels/PhanCongVM.cs
+++ PhoneShop/ViewModels/PhanCongVM.cs
@@ -5,7 +5,10 @@ namespace PhoneShop.ViewModels
     public class PhanCongVM
     {
         public PhanCong PhanCong { get; set; }
-        public List<NhanVien> NhanViens { get; set; } // Danh sách nhân viên
-        public List<PhongBan> PhongBans { get; set; } // Danh sách phòng ban
+
+        // Danh sách nhân viên và phòng ban không bao giờ null
+        public List<NhanVien> NhanViens { get; set; } = new List<NhanVien>();
+        public List<PhongBan> PhongBans { get; set; } = new List<PhongBan>();
     }
+
 }
