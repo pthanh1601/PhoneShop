eff21cb17bdace5dbb8ce46d09257f1cab33a0fc
thanhchan791791 <126339803+thanhchan791791@users.noreply.github.com>
2024-12-11 09:32:04 +0700

    new ajax

--- PhoneShop/Controllers/HangHoasController.cs
+++ PhoneShop/Controllers/HangHoasController.cs
@@ -70,7 +70,39 @@ namespace PhoneShop.Controllers
 
             return View(result);
         }
+        [HttpPost]
+        public async Task<IActionResult> GetProducts(int? loai, int page = 1)
+        {
+            int pageSize = 3; // Số sản phẩm mỗi trang
 
+            var hangHoas = db.HangHoas.AsQueryable();
+
+            if (loai.HasValue)
+            {
+                hangHoas = hangHoas.Where(p => p.MaLoai == loai.Value);
+            }
+
+            // Chọn các trường cần thiết
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHh = p.MaHh,
+                TenHh = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MotaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+
+            var paginatedList = await PaginatedList<HangHoaVM>.CreateAsync(result, page, pageSize);
+
+            return Json(new
+            {
+                data = paginatedList,
+                totalPages = paginatedList.TotalPages,
+                currentPage = page,
+                totalCount = paginatedList.TotalCount
+            });
+        }
         // Action hiển thị chi tiết sản phẩm
         public IActionResult Detail(int id)
         {
